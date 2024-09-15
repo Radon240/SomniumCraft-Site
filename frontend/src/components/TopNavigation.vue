4fed24f0ac54c64b71fe3b35dff5af0e8188c212
Maxim Gampel <radon24code@gmail.com>
2024-09-15 21:50:50 +0300

    feat: Created components for top navigation

---
+++ frontend/src/components/TopNavigation.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+import TopNavigationButton from "./TopNavigationButton.vue";
+</script>
+
+<template>
+  <ul>
+    <li><TopNavigationButton content="Главная" /></li>
+    <li><TopNavigationButton content="Вики" /></li>
+    <li><TopNavigationButton content="Карта" /></li>
+    <li><TopNavigationButton content="Прочее"/></li>
+  </ul>
+</template>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 1rem 1rem;
+  overflow: hidden;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+li {
+  margin: 0.5rem;
+  flex: 1 1 auto;
+  min-width: fit-content;
+}
+</style>
