ff912b2cadd5719b063ecf484d62263ff6418ac6
Maxim Gampel <radon24code@gmail.com>
2024-09-15 21:52:30 +0300

    feat: Created and setup Header and Footer components

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+import TopNavigation from "./TopNavigation.vue";
+</script>
+
+<template>
+  <div>
+    <img src="/src/assets/Discord_Avatar_2024.svg" width=50 height=50 />
+    <TopNavigation />
+  </div>
+</template>
+
+<style scoped>
+</style>
