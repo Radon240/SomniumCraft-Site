ef85c0685a6a32e27a78bdd98174e80599b123e7
Radon24 <radon24code@gmail.com>
2024-09-19 20:39:02 +0300

    feat: Using vue-router to navigate between pages

---
+++ frontend/src/app/router.ts
@@ -0,0 +1,27 @@
+import {computed, ref} from "vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import WikiPage from "..//pages/wiki/ui.vue"
+
+const routes= [
+    //{ path: '/main', name: 'MainPage', component: MainPage },
+    { path: '/wiki', name: 'WikiPage', component: WikiPage },
+    { path: '/resources', name: 'Resources'}
+]
+
+const currentPath = ref(window.location.hash)
+
+window.addEventListener('hashchange', () => {
+    currentPath.value = window.location.hash
+})
+
+const currentView = computed(() => {
+    return routes[currentPath.value.slice(1) || '/'] || NotFound
+})
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+
+export default router;
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import './app/style.scss'
 import App from './app/App.vue'
+import router from './app/router.ts'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faYoutube, faDiscord } from '@fortawesome/free-brands-svg-icons'
+
+library.add(faBars, faYoutube, faDiscord)
 
 const app = createApp(App).mount('#app')
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.use(router).mount('#app')
