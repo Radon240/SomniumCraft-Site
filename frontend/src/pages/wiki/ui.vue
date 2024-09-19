a7fce7867255b363318a6b2e900fa694c6160dba
Radon24 <radon24code@gmail.com>
2024-09-19 20:43:20 +0300

    refactor: Setup wiki page

--- frontend/src/pages/wiki/ui.vue
+++ frontend/src/pages/wiki/ui.vue
@@ -1,45 +1,24 @@
 <script setup lang="ts">
-
-import BaseLayout from "../../widgets/base-layout/ui.vue";
-import Footer from "../../widgets/footer/ui.vue";
-import Header from "../../widgets/header/ui.vue";
+import WikiContent from "../../widgets/wiki-content/ui.vue"
 </script>
 
 <template>
-  <BaseLayout>
-    <template #header>
-      <Header class="header" />
-    </template>
-
-    <template #default>
-
-    </template>
-
-    <template #footer>
-      <Footer class="footer"/>
-    </template>
-  </BaseLayout>
+    <div class="content">
+      <WikiContent />
+    </div>
 </template>
 
 <style scoped>
-.header{
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-direction: initial;
-  font-size: 1rem;
-  height: 3rem;
-  top: 0;
-  position: sticky;
-  background: var(--background-color);
-}
-.footer{
+
+.content{
+  position: relative;
+  margin-top: 7rem;
+  min-height: calc(100vh - 5rem);
   display: flex;
   justify-content: space-evenly;
-  align-items: center;
+  align-items: start;
   flex-direction: row;
-  height: 2rem;
   background: var(--background-color);
-  bottom: 0;
 }
+
 </style>
