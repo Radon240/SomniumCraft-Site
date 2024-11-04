058418c90c95ddd57e14a12a742785cc9798b1d7
Maxim Gampel <radon24code@gmail.com>
2024-11-04 17:48:53 +0300

    feat: setup router

--- frontend/src/app/router.ts
+++ frontend/src/app/router.ts
@@ -1,27 +1,34 @@
-import {computed, ref} from "vue";
-import { createRouter, createWebHistory } from 'vue-router';
-import WikiPage from "..//pages/wiki/ui.vue"
+import { computed, ref } from "vue";
+import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router';
 
-const routes= [
-    //{ path: '/', name: 'MainPage', component: MainPage },
-    { path: '/', name: 'wiki', component: WikiPage },
-    { path: '/resources', name: 'Resources'}
-]
+import MainPage from "@/pages/main-page.vue";
+import BlogPage from "@/features/blog/blog.vue";
+import NewsPage from "@/features/news/news.vue";
+import WikiPage from "@/pages/wiki/ui.vue";
+import NotFound from "@/pages/404.vue";
 
-const currentPath = ref(window.location.hash)
+const routes: Array<RouteRecordRaw> = [
+    { path: '/', redirect: '/main' },
+    { path: '/main', name: 'MainPage', component: MainPage },
+    { path: '/wiki', name: 'WikiPage', component: WikiPage },
+    { path: '/blog', name: 'BlogPage', component: BlogPage },
+    { path: '/news', name: 'NewsPage', component: NewsPage },
+    { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound }
+];
+
+const currentPath = ref<string>(window.location.hash);
 
 window.addEventListener('hashchange', () => {
-    currentPath.value = window.location.hash
-})
+    currentPath.value = window.location.hash;
+});
 
-const currentView = computed(() => {
-    return routes[currentPath.value.slice(1) || '/'] || NotFound
-})
+const currentView = computed((): typeof NotFound => {
+    return routes[currentPath.value.slice(1) || '/'] || NotFound;
+});
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
 });
-
 
 export default router;
