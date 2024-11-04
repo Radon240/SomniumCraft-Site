cb591dd564583516bc1625c6ec0afccc7c1edb84
Maxim Gampel <radon24code@gmail.com>
2024-11-04 17:56:19 +0300

    feat: setup App.vue

--- frontend/src/app/App.vue
+++ frontend/src/app/App.vue
@@ -4,24 +4,62 @@ import Footer from '@/widgets/Footer.vue';
 import {computed} from 'vue';
 import {useRoute} from 'vue-router';
 
-import Header from "..//widgets/header/ui.vue"
-import Footer from "..//widgets/footer/ui.vue"
-
+const route = useRoute();
+const isMainRoute = computed(() => route.path === '/main');
 </script>
 
 <template>
   <div id="app">
+    <div v-show="isMainRoute" class="background-image"></div>
     <Header />
-    <router-view />
+    <main>
+      <router-view />
+    </main>
     <Footer />
   </div>
 </template>
 
 <style scoped>
+
+html, body{
+  min-width: 100%;
+  margin: 0;
+}
 #app {
+
+  min-width: 100%;
   display: flex;
   flex-direction: column;
   min-height: 100vh; /* Ensure the app takes at least the full viewport height */
+
+
+  overflow-x: hidden/* Ensure the background image is positioned relative to this container */
+
 }
 
+.background-image {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh; /* Cover the whole viewport height */
+  background-image: url("@/resources/images/backgroundIce.png"); /* Укажите путь к вашему изображению */
+  background-repeat: no-repeat;
+  background-position: center; /* Центрирование изображения */
+  background-size: cover; /* Сделать изображение адаптивным */
+  z-index: -1; /* Поместить фоновое изображение под другими элементами */
+}
+
+header {
+  /* Styling for the header if needed */
+}
+
+main {
+  position: relative;
+  min-height: 100vh;
+}
+
+footer {
+  /* Styling for the footer if needed */
+}
 </style>
