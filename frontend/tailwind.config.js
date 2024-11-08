d40c91bc0b8d87b1f69b1add28e9d8ca15b9a361
EVER-ON <gurkadima2004@bk.ru>
2024-11-09 01:57:20 +0300

    feat: add library tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,31 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'], // Используем 'content' вместо 'purge'
+  darkMode: 'media', // Либо 'media', либо удалите эту строку, если не хотите использовать темную тему
   theme: {
+    extend: {
+      colors: {
+          'pastel-blauw': '#699BF6'
+      }
+    },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
 }
-
