117400e7bf82806d95aa88f59b0887cd654c280b
Maxim Gampel <radon24code@gmail.com>
2024-11-10 19:11:09 +0300

    feat: fetching and display articles from navTree

---
+++ frontend/src/entities/Article/ArticleStore.ts
@@ -0,0 +1,41 @@
+import { defineStore } from 'pinia'
+import {wikiApi} from "@/shared/api/wikiApiService.ts";
+import {GetArticleResponseAuthor, GetArticleResponseCategory, ReviewStatus} from "@/shared/api";
+
+interface Article {
+    id: string;
+    title: string;
+    content: string | null;
+    contributors: GetArticleResponseAuthor[];
+    /** @format uuid */
+    revisionId: string | null;
+    reviewStatus: ReviewStatus;
+    /** @format date-time */
+    submittedTimestamp: string | null;
+    /** @format date-time */
+    reviewTimestamp: string | null;
+    categories: GetArticleResponseCategory[];
+}
+
+export const useArticleStore = defineStore('article', {
+    state: () => ({
+        article: null as Article | null,
+        loading: false,
+        error: null as string | null,
+    }),
+    actions: {
+        async fetchArticle(articleTitle: string) {
+            this.loading = true
+            this.error = null
+            try {
+                const response = await wikiApi.api.getArticle(articleTitle)
+                this.article = response.data
+            } catch (err) {
+                this.error = 'Failed to load article'
+                console.error(err)
+            } finally {
+                this.loading = false
+            }
+        },
+    },
+})
